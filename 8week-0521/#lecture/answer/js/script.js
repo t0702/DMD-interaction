0250063247155624bec288d0713760b666f210c6
t0702 <t0702@naver.com>
2020-05-28 17:41:17 +0900

    lecture09

--- 8week-0521/#lecture/answer/js/script.js
+++ 8week-0521/#lecture/answer/js/script.js
@@ -3,7 +3,7 @@ Quest 1.
 1. 문서의 title을 '학번_이름' 으로 변경합니다.
 */
 // Answer 1.
-
+document.title = '21961086_이유정'; // set.
 
 
 /*
@@ -21,6 +21,18 @@ Quest 2.
 ----------
 */
 // Answer 2.
+function getSize(){
+    var winWidth = window.innerWidth; // 브라우저 윈도우의 가로 너비.
+    var winHeight = window.innerHeight; // 브라우저 윈도우의 세로 높이.
+    
+    return {'winWidth' : winWidth, 'winHeight' : winHeight} // 객체 반환.
+    console.Log('함수 반환 이후 코드는 동작하지 않음.');
+}
+var winSize = getSize(); // 함수에서 반환된 객체가 값으로 할당.
+// console.log(winSize);
+var result = "윈도우 콘텐츠의 영역 width : " + winSize.winWidth + " px, height : " + winSize['winHeight'] + " px 입니다.";
+console.log(result);
+
 
 
 
@@ -29,14 +41,48 @@ Quest 3.
 1. id 가 'title' 인 요소에 'Hello World' 문자열이 포함되도록 작성합니다.
 */
 // Answer 3.
+var titleEl = document.getElementById('title');
+titleEl.innerText = 'Hello World'; // 요소에 텍스트만 기입
+titleEl.innerHTML = 'Hello World'; // 요소에 텍스트만 기입
+titleEl.innerHTML = '<span>Hello World</span>'; // 요소에 HTML 형식의 텍스트 기입.
+
+var html = '';
+
+html += "<div class =\"class\">";
+html +=     "<span>";
+html +=         "안녕하세요";
+html +=     "</span>";
+for(var i = 0; i < 5; i++){
+    html +=     "<span>";
+    html +=         "안녕하세요" + i;
+    html +=     "</span>";
+}
+html += "</div>";
+titleEl.innerHTML = html;
+
+console.log(html);
 
 
+// var spanEl = document.createElement('span');
+// spanEl.innerText = 'Hello World';
+// titleEl,appendChild(spanEl);
 
 /*
 Quest 4.
 1. class 가 'title' 인 요소들에 '안녕하세요.' 문자열이 포함되도록 작성합니다.
 */
 // Answer 4.
+// id는 문서에서 오로지 1개만 존재 가능.
+// class는 한 개 이상 존재 가능.
+var classEls = document.getElementsByClassName('title'); // 찾아온 클래스 요소들이 배열로 반환
+// console.log(classEls);
+// getElements~ 로 찾아온 반환 요소에 접근하기 위해서는 반복문을 사용해아 함.
+for(var i = 0; i < classEls.length; i++){
+    var classEl = classEls[i]; // 각 요소 노드 접근
+    classEls[i].innerText = '안녕하세요.'; // 새로운 문자열이 요소에 기입
+    classEls[i].innerHTML = '안녕하세요.';
+    classEl.innerText = '안녕하세요.';
+}
 
 
 
@@ -51,6 +97,15 @@ row class의 개수는 __개 입니다.
 ----------
 */
 // Answer 5.
+function getRowCount(){
+    var row = document.getElementsByClassName('row');
+    var rowCount = row.length;
+    return rowCount;
+}
+var rowCount = getRowCount();
+console.log(rowCount);
+var result = "row class의 개수는 " + rowCount + "개 입니다."
+console.log(result);
 
 
 
@@ -61,6 +116,22 @@ Quest 6.
 3. 10 번의 출력 후 해당 기능이 실행되지 않도록 작성합니다.
 */
 // Answer 6.
+var _count = 0;
+var _time = 1000;
+function onCount(){ // 타이머 callback에 해당하는 핸들러 정의
+    _count++; // count 변수에 + 1하여 연산
+    console.log('hello', _count);
+    if(_count >= 10){
+        clearInterval(timer); // 타이머의 초기화
+    }
+    // cleaInterval(timer);
+}
+var timer = setInterval(onCount, _time);
+// var timer = setInterval(function(){
+//    code...
+// }, 1000);
+// setInterval(callback, time);
+// time - 1000 : 초  / 1000 : 10초 ms 단위
 
 
 
@@ -70,6 +141,11 @@ Quest 7.
 2. 1(순서) 요소의 위치를 x : 400px, y : 0px 로 좌표이동합니다.
 */
 // Answer 7.
+var scroll = document.getElementById('scroll');
+scroll.classList.add('scroll');
+setTimeout(function(){
+    scroll.scrollTo(400, 500);
+}, 10); // index.html 이 로드된 후 스크롤이 이동되지 않을 수 있기 때문에 약간으이 딜레이를 주는 방법.
 
 
 
@@ -80,7 +156,13 @@ Quest 8.
 3. 1(순서) 요소의 자식 중 세번째 요소(.row)를 삭제합니다.
 */
 // Answer 8.
-
+var article = document.getElementById('article');
+var rowEls = article.getElementsByClassName('row');
+rowEls = article.querySelectorAll('.row');
+console.log(rowEls);
+rowEls[0].classList.add('red'); // 클래스를 추가하는 방법.
+// rowEls[2].classList.remove('row');
+article.removeChild(rowEls[2]); // 자식요소를 삭제하는 방법
 
 
 /*
@@ -102,5 +184,20 @@ Quest 9.
 7. enterName 함수를 호출합니다.
 */
 // Answer 9.
-
-
+function enterName(){
+    var name = prompt('이름을 입력해주세요.');
+    // console.log(name);
+    if(name === null){
+        console.log('입력이 취소되었습니다.');
+        return; // 이후의 코드가 동작하지 않도록 반환
+    }
+    if(name === ''){ 
+        // 입력창에 빈 문자열 값이 있는 경우
+        alert('이름을 정확히 입력해주세요.');
+        enterName();
+    } else {
+        // 입력창에 문자열 값이 있는 경우
+        document.write(name); // body 요소의 마지막 부분에 문자열 출력.
+    }
+}
+enterName();
---
+++ 9week-0528/#lecture/answer/js/script.js
@@ -0,0 +1,263 @@
+/*
+Quest 1.
+1. button#btn-student-number 요소에 클릭 이벤트를 추가하고 이벤트 핸들러를 할당합니다.
+2. 1(순서) 이벤트 호출 시 자신의 학번이 span#student-number 의 내부에 입력되고, 아래와 같은 메시지 결과값을 경고창으로 출력되도록 작성합니다.
+----------
+메시지)
+학번이 입력되었습니다.
+----------
+3. button#btn-student-name 요소에 클릭 이벤트를 추가하고 이벤트 핸들러를 할당합니다.
+4. 3(순서) 이벤트 호출 시 자신의 이름이 span#student-name 의 내부에 입력되고, 아래와 같은 메시지 결과값을 경고창으로 출력되도록 작성합니다.
+----------
+메시지)
+이름이 입력되었습니다.
+----------
+*/
+// Answer 1.
+var btnStuNum = document.querySelector('button#btn-student-number');
+btnStuNum = document.getElementById('btn-student-number');
+function OnStuNum(e){ // 핸들러(handler) 정의
+    e.preventDefault(); // 요소가 가지고 있는 기본 이벤트 동작을 방지
+    console.log(e); // 이벤트 정보에 대한 객체(내장 변수 객체)
+    // 클릭 이벤트가 발생되면 코드가 동작
+    var spanStuNum = document.querySelector('span#student-number');
+    spanStuNum.innerText = '21961086';
+    alert('학번이 입력되었습니다.');
+}
+btnStuNum.addEventListener('click', OnStuNum);
+
+var btnStuName = document.querySelector('button#btn-student-name');
+function OnStuName(e){
+    e.preventDefault();
+    var spanStuName = document.querySelector('span#student-name');
+    spanStuName.innerHTML = '<b>이유정</b>';
+    alert('이름이 입력되었습니다.')
+}
+btnStuName.addEventListener('click', OnStuName);
+
+
+
+/*
+Quest 2.
+1. button#btn-window-size 요소에 클릭 이벤트를 추가하고 이벤트 핸들러를 할당합니다.
+2. 1(순서) 이벤트 호출 시 윈도우 콘텐츠 영역의 너비 값과 높이 값을 아래와 같은 문자열 결과값으로 console 에 출력되도록 작성합니다.
+----------
+문자열)
+윈도우 콘텐츠의 영역 width : ____ px, height : ____ px 입니다.
+----------
+*/
+// Answer 2.
+var btnWinSize = document.querySelector('button#btn-window-size');
+function OnSize(e){
+    e.preventDefault();
+    var winWidth = window.innerWidth;
+    var winHeight = window.innerHeight;
+    var result = '윈도우 컨텐츠의 영역 width : ' + winWidth + 'px, height : ' + winHeight + 'px 입니다.';
+    console.log(result);
+}
+btnWinSize.addEventListener('click', OnSize );
+
+
+
+/*
+Quest 3.
+1. button#btn-check-id 요소에 클릭 이벤트를 추가하고 이벤트 핸들러를 할당합니다.
+2. 1(순서) 이벤트 호출 시 id 가 'user_id' 인 요소를 찾고 value 속성의 값의 문자열 개수를 아래와 같은 문자열 결과값으로 console 에 출력되도록 작성합니다.
+----------
+문자열)
+input#user_id - value 속성 값의 문자열 개수는 ____ 입니다.
+----------
+*/
+// Answer 3.
+var btnCheckId = document.querySelector('button#btn-check-id');
+function OnCheckId(e){
+    e.preventDefault();
+    var userId = document.getElementById('user_id');
+    var value = userId.value; // input 요소에서 value 값을 찾는 걸 주로 사용되기 때문에 해당 방법응로 찾을 수 있다.
+    var max = value.length;
+    // 현재 input 에서 값이 변경되면 해당 값을 반환
+    // value = userId.getAttribute('value'); // 문서 속성에 기입된 값을 반환
+    // console.log(value);
+    var result = 'input#user_id - value 속성 값의 문자열 개수는 '+ max +'입니다.';
+    console.log(result);
+}
+btnCheckId.addEventListener('click', OnCheckId);
+
+
+
+/*
+Quest 4.
+1. button#btn-cancel 요소에 클릭 이벤트를 추가하고 이벤트 핸들러를 할당합니다.
+2. 1(순서) 이벤트 호출 시 현재 브라우저 창이 새로고침 되도록 작성합니다.
+*/
+// Answer 4.
+var btnCancel = document.querySelector('button#btn-cancel');
+function OnCancel(e){
+    e.preventDefault();
+    window.location.reload();
+    // location.reload();
+    // 현재 문서를 새로고침.
+}
+btnCancel.addEventListener('click', OnCancel);
+
+
+/*
+Quest 5.
+1. button#btn-apply 요소에 클릭 이벤트를 추가하고 이벤트 핸들러를 할당합니다.
+2. 1(순서) 이벤트 호출 시 '가입하시겠습니까?' 라는 메세지를 포함하고 있는 확인창을 출력합니다.
+3. 사용자가 확인을 선택한 경우, 아래와 같은 메시지 결과값을 경고창으로 출력되도록 작성합니다.
+----------
+메시지)
+가입을 환영합니다!
+----------
+4. 사용자가 취소를 선택한 경우, 아래와 같은 메시지 결과값을 경고창으로 출력되도록 작성합니다.
+----------
+메시지)
+가입을 취소하였습니다.
+----------
+*/
+// Answer 5.
+var btnApply = document.querySelector('button#btn-apply');
+function OnApply(e){
+    e.preventDefault();
+    var isApply = confirm('가입하시겠습니까?');
+    if(isApply === true){
+        alert('가입을 환영합니다!');
+    } else {
+        alert('가입을 취소하였습니다.');
+    }
+
+    /* 
+    // boolean 일 때 조건문 표기법
+
+    if(!isApply){ //false
+        
+    } else { // true
+        
+    }
+     */
+}
+btnApply.addEventListener('click', OnApply);
+
+
+
+/*
+Quest 6.
+1. button#btn-reset 요소에 클릭 이벤트를 추가하고 이벤트 핸들러를 할당합니다.
+2. 1(순서) 이벤트 호출 시 '초기화하시려면 100을 입력하세요.' 라는 문구와 입력창이 포함된 다이얼로그창을 출력합니다.
+3. 사용자가 100을 정확하게 입력할 경우, input#user_id 와 input#user_pass 요소의 value 속성이 빈 값으로 초기화 되도록 작성합니다.
+*/
+// Answer 6.
+var btnReset = document.querySelector('button#btn-reset');
+function OnReset(e){
+    e.preventDefault();
+    var reset100 = prompt('초기화하시려면 100을 입력하세요.');
+    // prompt - 취소 : null / 확인 : 현재 입력된 문자열 값 
+    // prompt는 데이터 타임이 무조건 문자열
+    /* 
+    if(reset100 == 100){ // 문자열 100이라도 true가 됨 
+    }
+    if(reset100 === 100){ // false - prompt의 숫자가 100이지만 문장열 타입의 데이터이기 때문에 false
+    }
+     */
+    if(parseInt(reset100, 10) === 100) {
+        // parseInt(값(문자열), 진법);
+        // parseInt('100') 기본이 10진법.
+        // parseInt('100', 10); 문자열 '100'을 숫자(10진법의) 타입으로 변경한다.
+        // parseInt('100', 2); 문자열 '100'을 2진법의 숫자로 변경.
+        var userIdEl = document.querySelector('input#user_id');
+        var userPassEl = document.querySelector('input#user_pass');
+        userIdEl.value = '';
+        userPassEl.value = '';
+    }
+}
+btnReset.addEventListener('click', OnReset);
+
+/*
+Quest 7.
+1. button#btn-odd 요소에 클릭 이벤트를 추가하고 이벤트 핸들러를 할당합니다.
+2. 1(순서) 이벤트 호출 시 '숫자를 입력해주세요.' 라는 문구와 입력창이 포함된 다이얼로그창을 출력합니다.
+3. 사용자가 입력한 수를 기준으로 0부터 해당 숫자까지의 숫자 중 홀수인 숫자를 아래와 같이 문자열 연산하여 결과를 div#odd-result 의 내부에 입력될 수 있도록 작성합니다.
+----------
+메시지)
+문자열 연산 예시)
+2(순서) 다이얼로그창에 입력한 숫자가 10인 경우 : 1, 3, 5, 7, 9.
+2(순서) 다이얼로그창에 입력한 숫자가 5인 경우 : 1, 3, 5.
+----------
+*/
+// Answer 7.
+var btnOdd = document.querySelector('button#btn-odd');
+function OnOdd(e){
+    e.preventDefault();
+    var isOdd = prompt('숫자를 입력해주세요.');
+    isOdd = parseInt(isOdd, 10);
+
+    // '안녕하세요';
+    // parseInt('안녕하세요'); -> NaN (수식이 성립되지 않는다)
+
+    if( isOdd !== null && isOdd !== '' && parseInt(isOdd, 10) !== NaN){
+        isOdd = parseInt(isOdd, 10);
+        var result = '';
+        for(var i = 0; i <= isOdd; i++){
+            if ( i % 2 == 1){ // 홀수
+                if(result !== ''){ // result의 값이 빈 문자열이 아닌 경우, 홀수 여부를 한 번은 확인하여 연산이 이루어짐.
+                    result += ', ';
+                }
+                result += i;
+            } else { // 짝수
+
+            }
+            if(i === isOdd){ // i가 맨 마지막이 될 때 
+                result += '.'; //마침표를 찍음.
+            }
+        }
+        var oddResult = document.querySelector('div#odd-result');
+        oddResult.innerText = result;
+    }
+}
+btnOdd.addEventListener('click', OnOdd);
+
+
+
+
+/*
+Quest 8.
+1. button#btn-add-city 요소에 클릭 이벤트를 추가하고 이벤트 핸들러를 할당합니다.
+2. 1(순서) 이벤트 호출 시 '도시명을 입력하세요.' 라는 문구와 입력창이 포함된 다이얼로그창을 출력합니다.
+3. 사용자가 도시명을 입력할 경우, ul#city-list 자식인 li 요소에 도시명 문자열이 추가되도록 작성합니다.
+*/
+// Answer 8.
+
+
+
+/*
+Quest 9.
+1. button#btn-color 요소에 클릭 이벤트를 추가하고 이벤트 핸들러를 할당합니다.
+2. 1(순서) 이벤트 호출 시 sentence 라는 클래스 이름을 가진 모든 p 요소의 글자 색 스타일을 'red' 로 변경합니다.
+3. 단, 1(순서) 이벤트 호출 시 클릭된 횟수를 체크하여 현재 횟수에 위치하는 p 요소만 예외로 글자 색 스타일을 'black' 으로 변경합니다.(아래 예시 참고)
+4. 1(순서) 이벤트가 여러 번 호출되어 마지막 p 요소가 예외 처리된 상황(글자 색 스타일 'black')에서 1(순서) 이벤트 호출 시 처음 위치한 p 요소부터 예외 처리 되도록 작성합니다.
+----------
+* 예시 - p요소의 전체 개수가 4개일 때, button#btn-color 클릭 횟수에 따른 p 요소 스타일 변경
+
+횟수 1 : p요소(0) - 'black', p요소(1) - 'red', p요소(2) - 'red', p요소(3) - 'red'
+횟수 2 : p요소(0) - 'red', p요소(1) - 'black', p요소(2) - 'red', p요소(3) - 'red'
+횟수 3 : p요소(0) - 'red', p요소(1) - 'red', p요소(2) - 'black', p요소(3) - 'red'
+횟수 4 : p요소(0) - 'red', p요소(1) - 'red', p요소(2) - 'red', p요소(3) - 'black'
+횟수 5 : p요소(0) - 'black', p요소(1) - 'red', p요소(2) - 'red', p요소(3) - 'red'
+횟수 6 : p요소(0) - 'red', p요소(1) - 'black', p요소(2) - 'red', p요소(3) - 'red'
+...
+...
+----------
+*/
+// Answer 9.
+
+
+
+/*
+Quest 10.
+1. button#btn-go-top 요소에 클릭 이벤트를 추가하고 이벤트 핸들러를 할당합니다.
+2. 1(순서) 이벤트 호출 시 스크롤이 현재 화면의 최상단으로 이동되도록 작성합니다.
+*/
+// Answer 10.
+
+
